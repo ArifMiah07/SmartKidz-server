abae8b7330b5c790c7d5f2ed5171fff69eecb921
ArifMiah07 <arifmiah.me322@gmail.com>
2024-05-20 18:34:33 +0600

    added curd oparetiona and get serviceProviderInfo in client side

--- index.js
+++ index.js
@@ -8,19 +8,6 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-/**
- * //Must remove "/" from your production URL
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://cardoctor-bd.web.app",
-      "https://cardoctor-bd.firebaseapp.com",
-    ],
-    credentials: true,
-  })
-);
- */
 app.use(
     cors({
         origin: [
@@ -79,15 +66,15 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
 
-        const serviceCollection = client.db('carDoctor').collection('services');
-        const bookingCollection = client.db('carDoctor').collection('bookingsData');
+        // const serviceCollection = client.db('carDoctor').collection('services');
+        // const bookingCollection = client.db('carDoctor').collection('bookingsData');
         const aboutUsCollection = client.db('carDoctor').collection('aboutUs');
         const aboutClassesCollection = client.db('carDoctor').collection('aboutClasses');
         const aboutCoursesCollection = client.db('carDoctor').collection('aboutCourses');
         const categoryCollection = client.db('carDoctor').collection('category');
         const eventsCollection = client.db('carDoctor').collection('events');
-        const addedServiceCollection = client.db('carDoctor').collection('addService');
-        const bookingsCollection = client.db('carDoctor').collection('bookings');
+        const addedServiceCollection = client.db('carDoctor').collection('addService');//service provers added services 
+        const bookingsCollection = client.db('carDoctor').collection('bookings');//users purchase
         const serviceProviderInfoCollection = client.db('carDoctor').collection('ProviderInfo');
 
         // auth related api
@@ -135,6 +122,18 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         })
+        app.get('/category/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+
+            const options = {
+                // Include only the `title` and `imdb` fields in the returned document
+                projection: { title: 1, price: 1, service_id: 1, img: 1 },
+            };
+
+            const result = await categoryCollection.findOne(query, options);
+            res.send(result);
+        })
         //events api
         app.get('/events', async (req, res)=>{
             const cursor = eventsCollection.find();
@@ -155,11 +154,40 @@ async function run() {
             res.send(result);
         });
 
+        app.get('/serviceProviderInfo', async(req, res)=>{
+            const cursor = serviceProviderInfoCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+            
+        })
 
 
         
 
         
+        app.get('/add-services', logger, async (req, res) => {
+                const cursor = addedServiceCollection.find();
+                const result = await cursor.toArray();
+                res.send(result);
+        })
+
+
+        // bookings add services
+        app.get('/add-services', logger, verifyToken, async (req, res) => {
+            console.log(req.query.email);
+            console.log('ttttt token', req.cookies.token)
+            console.log('user in the valid token', req.user)
+            if(req.query.email !== req.user.email){
+                return res.status(403).send({message: 'forbidden access'})
+            }
+            
+            let query = {};
+            if (req.query?.email) {
+                query = { email: req.query.email }
+            }
+            const result = await addedServiceCollection.find(query).toArray();
+            res.send(result);
+        })
         //add a new service api
         app.post('/add-service', async (req, res) => {
             const booking = req.body;
@@ -167,81 +195,8 @@ async function run() {
             const result = await addedServiceCollection.insertOne(booking);
             res.send(result);
         });
-        app.get('/add-services', logger, async (req, res) => {
-            const cursor = addedServiceCollection.find();
-            const result = await cursor.toArray();
-            res.send(result);
-        })
-
-        // app.get('/add-services/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: new ObjectId(id) }
-
-        //     const options = {
-        //         // Include only the `title` and `imdb` fields in the returned document
-        //         projection: { title: 1, price: 1, service_id: 1, img: 1 },
-        //     };
-
-        //     const result = await serviceCollection.findOne(query, options);
-        //     res.send(result);
-        // })
-
-        // services related api
-        app.get('/services', logger, async (req, res) => {
-            const cursor = serviceCollection.find();
-            const result = await cursor.toArray();
-            res.send(result);
-        })
-
-        app.get('/services/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
-
-            const options = {
-                // Include only the `title` and `imdb` fields in the returned document
-                projection: { title: 1, price: 1, service_id: 1, img: 1 },
-            };
-
-            const result = await serviceCollection.findOne(query, options);
-            res.send(result);
-        })
-
-        //service booking  api
-        app.post('/bookings', async (req, res) => {
-            const bookings = req.body;
-            console.log(bookings);
-            const result = await bookingsCollection.insertOne(bookings);
-            res.send(result);
-        });
-
-
-        // bookings 
-        app.get('/bookings', logger, verifyToken, async (req, res) => {
-            console.log(req.query.email);
-            console.log('ttttt token', req.cookies.token)
-            console.log('user in the valid token', req.user)
-            if(req.query.email !== req.user.email){
-                return res.status(403).send({message: 'forbidden access'})
-            }
-
-            let query = {};
-            if (req.query?.email) {
-                query = { email: req.query.email }
-            }
-            const result = await bookingCollection.find(query).toArray();
-            res.send(result);
-        })
-        // app.post('/bookings', async (req, res) => {
-        //     try {
-        //       const booking = new Booking(req.body);
-        //       await booking.save();
-        //       res.status(201).send(booking);
-        //     } catch (error) {
-        //       res.status(400).send(error);
-        //     }
-        //   });
-
-        app.patch('/bookings/:id', async (req, res) => {
+        //update
+        app.patch('/add-services/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
             const updatedBooking = req.body;
@@ -251,16 +206,27 @@ async function run() {
                     status: updatedBooking.status
                 },
             };
-            const result = await bookingCollection.updateOne(filter, updateDoc);
+            const result = await addedServiceCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+        //delete
+        app.delete('/add-services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await addedServiceCollection.deleteOne(query);
             res.send(result);
         })
 
-        app.delete('/bookings/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
-            const result = await bookingCollection.deleteOne(query);
+
+
+        //service booking  api
+        app.post('/bookings', async (req, res) => {
+            const bookings = req.body;
+            console.log(bookings);
+            const result = await bookingsCollection.insertOne(bookings);
             res.send(result);
-        })
+        });
+
 
 
         // Send a ping to confirm a successful connection
